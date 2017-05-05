f4b76d177da3ba9c7755dea767ac67cddda82a74
Ivan Tomilov <tiabc.work@gmail.com>
2017-05-05 13:20:00 +0700

    Switched to gopkg.in/gomail.v2 for sending emails

--- lib/mail/mail.go
+++ lib/mail/mail.go
@@ -1,20 +1,39 @@
 package mail
 
-// TODO: Use gopkg.in/gomail.v2 instead of the default email sender.
-
 import (
 	"fmt"
+
 	"github.com/blue-jay/blueprint/lib/env"
+	"github.com/blue-jay/core/email"
 	"github.com/blue-jay/core/server"
+	"gopkg.in/gomail.v2"
 )
 
-// SendVerification sends email that a user needs to verify his email.
+// SendVerification sends email that a user needs to verify their email.
 func SendVerification(c env.Info, email, code string) error {
-	return c.Email.Send(
+	return Send(
+		c.Email,
 		email,
 		"Email verification",
 		"Click this link to verify your email: "+schemeHost(c.Server)+"/verify?code="+code,
 	)
+}
+
+// Send sends plaintext email.
+func Send(info email.Info, to, subject, body string) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", info.From)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/plain", body)
+
+	d := gomail.NewDialer(
+		info.Hostname,
+		info.Port,
+		info.Username,
+		info.Password,
+	)
+	return d.DialAndSend(m)
 }
 
 // schemeHost returns a canonical URL representation for external access.
